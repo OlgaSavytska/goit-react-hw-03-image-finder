6a705cf50b041803be5e1cbc3e825395787eae74
OlgaSavytska <o.savytska77@gmail.com>
2019-09-12 00:12:35 +0300

    Updates

---
+++ precache-manifest.13f1fc5b10464e5f581ea2bcb1e7e3c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "700a9ab95b602e54e52ad8e01bedebcf",
+    "url": "/goit-react-hw-03-image-finder-new/index.html"
+  },
+  {
+    "revision": "9b35d8e6b0f947db43aa",
+    "url": "/goit-react-hw-03-image-finder-new/static/css/main.d49dfa4d.chunk.css"
+  },
+  {
+    "revision": "071f0a74c0a3f27a524c",
+    "url": "/goit-react-hw-03-image-finder-new/static/js/2.2c589504.chunk.js"
+  },
+  {
+    "revision": "9b35d8e6b0f947db43aa",
+    "url": "/goit-react-hw-03-image-finder-new/static/js/main.db345e3f.chunk.js"
+  },
+  {
+    "revision": "ec913650fd2b81be1d02",
+    "url": "/goit-react-hw-03-image-finder-new/static/js/runtime~main.ff499476.js"
+  }
+]);
